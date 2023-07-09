3f9232b8e2bce6cbecf9561a428f5a7384b98a55
Vyacheslav Osin <osin@automacon.ru>
2023-07-09 17:57:57 +0300

    Update unpack.go

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,25 +8,23 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
-	var lastRune rune = 0
-	r0 := rune('0')
-	r9 := rune('9')
+	var lastRune rune
 	sb := strings.Builder{}
 
 	for _, k := range s {
-		if k >= r0 && k <= r9 {
-			if (lastRune >= r0 && lastRune <= r9 || lastRune == 0) && !(k == r0 && lastRune == r0) {
+		if k >= '0' && k <= '9' {
+			if (lastRune >= '0' && lastRune <= '9' || lastRune == 0) && !(k == '0' && lastRune == '0') {
 				return "", ErrInvalidString
-			} else if !(lastRune >= r0 && lastRune <= r9) {
+			} else if !(lastRune >= '0' && lastRune <= '9') {
 				sb.WriteString(strings.Repeat(string(lastRune), int(k)-int('0')))
 			}
-		} else if !(lastRune >= r0 && lastRune <= r9 || lastRune == 0) {
+		} else if !(lastRune >= '0' && lastRune <= '9' || lastRune == 0) {
 			sb.WriteString(string(lastRune))
 		}
 		lastRune = k
 	}
 
-	if !(lastRune >= r0 && lastRune <= r9) && lastRune > 0 {
+	if !(lastRune >= '0' && lastRune <= '9') && lastRune > 0 {
 		sb.WriteString(string(lastRune))
 	}
 	return sb.String(), nil
