a002c0389dcd1732d3213a456a128170515cac86
Vyacheslav Osin <osin@automacon.ru>
2023-07-09 16:37:46 +0300

    Update unpack.go

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,32 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var last_rune rune = 0
+	var r0 rune = '0'
+	var r9 rune = '9'
+	var sb strings.Builder
+	for _, k := range s {
+		if k >= r0 && k <= r9 {
+			if last_rune >= r0 && last_rune <= r9 || last_rune == 0 {
+				return "", ErrInvalidString
+			} else if !(last_rune >= r0 && last_rune <= r9) {
+				sb.WriteString(strings.Repeat(string(last_rune), int(k)-int('0')))
+			}
+		} else if !(last_rune >= r0 && last_rune <= r9 || last_rune == 0) {
+			sb.WriteString(string(last_rune))
+		}
+		last_rune = k
+	}
+
+	if !(last_rune >= r0 && last_rune <= r9) {
+		sb.WriteString(string(last_rune))
+	}
+
+	return sb.String(), nil
 }
