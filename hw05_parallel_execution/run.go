63bfaa35dbf953fc36f213fc45b5bad541f75d0f
Vyacheslav Osin <osin@automacon.ru>
2023-08-05 12:56:22 +0300

    HW05 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,44 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	// управляющие переменные:
+	var errCount int            // текущее кол-во ошибок, сделано для наглядности, вместо уменьшения m: m--
+	var curTask int             // индекс задачи из слайса задач, которая будет запущена при следующем запуске
+	if l := len(tasks); l < n { // если массив задач меньше, то не будем запускать лишние горутины
+		n = l
+	}
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{} // один мьютекс на два блока кода, 
+	// т.к. в первом блоке читается также переменная, которая изменяется во втором
+
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			for {
+				mu.Lock()  // залочим код перед чтением управляющих переменных, 
+				// т.к. думаю читать нужно тоже внутри лока, иначе можем получить "грязное чтение"
+				if errCount >= m || curTask >= len(tasks) { // если число ошибок достигло лимита или все задачи обработаны
+					mu.Unlock() // разблокировать и прекратить цикл обработки
+					break
+				}
+				myCurTask := curTask // сохранить индекс своей задачи
+				curTask++            // передвинуть индекс следующей запускаемой задачи
+				mu.Unlock()
+
+				err := tasks[myCurTask]() // запустить выполнние своей задачи
+				if err != nil { // если была ошибка, безопасно увеличть счетчик ошибок
+					mu.Lock()
+					errCount++
+					mu.Unlock()
+				}
+			}
+			wg.Done() // ждать окончание всех горутин
+		}()
+	}
+	wg.Wait() // <==
+
+	if errCount == m {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
